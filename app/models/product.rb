2f81c0155f75bed3c256cb31cdb290156761e2b1
Jacek Buczek <jabuczek@gmail.com>
2014-08-07 00:52:11 +0200

    Average_rating and validations for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,11 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates :description, :title, :price, presence: true
+  validates :price, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }
+
+  def average_rating
+    reviews.map(&:rating).reduce(:+).to_f / reviews.size
+  end
 end
