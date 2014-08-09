06f9c3822a85a37ffc40c80ce197c3a2810c60ed
Jacek Buczek <jabuczek@gmail.com>
2014-08-09 12:29:03 +0200

    Changed routes for delete product

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :categories do
-    resources :products, except: [:index] do
+    resources :products, except: [:index, :delete] do
       resources :reviews
     end
   end
-  resources :products, only: [:index]
+  resources :products, only: [:index, :delete]
 
   root 'categories#index'
 end
