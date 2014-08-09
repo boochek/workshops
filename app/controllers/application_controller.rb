0c108f194ae1b4e24a4214cb774caccad15762e4
Jacek Buczek <jabuczek@gmail.com>
2014-08-09 13:56:30 +0200

    Improved devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,13 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   before_filter :authenticate_user!
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << [:firstname, :lastname, :admin]
+  end
 end
